a961f732361729c6ad44e4834a696629069368d6
aiu27 <88765710+aiu27@users.noreply.github.com>
2022-02-20 19:56:02 +0900

    L5-end

--- main.js
+++ main.js
@@ -5,10 +5,14 @@ const app = Vue.createApp({
       description: "Animal printed pair of socks",
       image: "../assets/images/socks_green.jpg",
       url: "https://www.happysocks.com/jp/",
-      inventory: 0,
-
+      inventory: 100,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        { id: 2234, color: "green" },
+        { id: 2235, color: "blue" },
+      ],
       //code challenge
-      onSale: true,
+      sizes: [22, 23, 24, 25],
     };
   },
 });
