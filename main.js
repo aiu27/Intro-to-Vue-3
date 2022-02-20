350c8c24274fc1ed84dc1181f093c5d7971ede90
aiu27 <88765710+aiu27@users.noreply.github.com>
2022-02-20 20:36:34 +0900

    L6-end

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
   data() {
     return {
+      cart: 0,
       product: "Socks",
       description: "Animal printed pair of socks",
       image: "../assets/images/socks_green.jpg",
@@ -8,11 +9,24 @@ const app = Vue.createApp({
       inventory: 100,
       details: ["50% cotton", "30% wool", "20% polyester"],
       variants: [
-        { id: 2234, color: "green" },
-        { id: 2235, color: "blue" },
+        { id: 2234, color: "green", image: "../assets/images/socks_green.jpg" },
+        { id: 2235, color: "blue", image: "../assets/images/socks_blue.jpg" },
       ],
       //code challenge
       sizes: [22, 23, 24, 25],
     };
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    removeFromCart() {
+      if (this.cart >= 1) {
+        this.cart -= 1;
+      }
+    },
+    updateImage(variantImage) {
+      this.image = variantImage;
+    },
   },
 });
