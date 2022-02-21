4a81cf462b82d5036ac6517aeda0a72247714515
aiu27 <88765710+aiu27@users.noreply.github.com>
2022-02-21 19:12:56 +0900

    L8-end

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
 const app = Vue.createApp({
   data() {
     return {
+      // code challenge
+      onSale: true,
+
       cart: 0,
       product: "Socks",
+      brand: "Vue Mastery",
       description: "Animal printed pair of socks",
-      image: "../assets/images/socks_green.jpg",
+      selectedVariant: 0,
       url: "https://www.happysocks.com/jp/",
-      inventory: 0,
       details: ["50% cotton", "30% wool", "20% polyester"],
       variants: [
-        { id: 2234, color: "green", image: "../assets/images/socks_green.jpg" },
-        { id: 2235, color: "blue", image: "../assets/images/socks_blue.jpg" },
+        {
+          id: 2234,
+          color: "green",
+          image: "../assets/images/socks_green.jpg",
+          quantity: 50,
+        },
+        {
+          id: 2235,
+          color: "blue",
+          image: "../assets/images/socks_blue.jpg",
+          quantity: 0,
+        },
       ],
       //code challenge
       sizes: [22, 23, 24, 25],
@@ -25,8 +38,25 @@ const app = Vue.createApp({
         this.cart -= 1;
       }
     },
-    updateImage(variantImage) {
-      this.image = variantImage;
+    updateVariant(index) {
+      this.selectedVariant = index;
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+    inventory() {
+      return this.variants[this.selectedVariant].quantity;
+    },
+    saleStatus() {
+      if (this.onSale) {
+        return this.brand + " " + this.product + "is on sale";
+      }
+      return " ";
     },
   },
 });
